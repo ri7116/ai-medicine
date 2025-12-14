74d731794926f50042d96bac7af9e2a01a7291c0
ri7116 <ri711628@gmail.com>
2025-12-14 20:42:36 +0900

    feat: firebase 연동 ChatListViewModel

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,6 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -16,9 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = uri("https://maven.google.com")
-        }
     }
 }
 
