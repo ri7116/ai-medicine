fc6e55430f277c5ac356eaa13b18f4012e047f7e
ri7116 <ri711628@gmail.com>
2025-12-17 01:10:18 +0900

    fix: isUser오류

--- app/src/main/java/com/example/chatbot/features/chatbot/data/ChatMessageUiModel.kt
+++ app/src/main/java/com/example/chatbot/features/chatbot/data/ChatMessageUiModel.kt
@@ -1,8 +1,14 @@
 package com.example.chatbot.features.chatbot.data
 
+import com.google.firebase.firestore.IgnoreExtraProperties
+import com.google.firebase.firestore.PropertyName
+
 // 채팅 메시지 하나를 나타내는 데이터 구조
+@IgnoreExtraProperties
 data class ChatMessageUiModel(
     val message: String = "",
-    val isUser: Boolean = false,
+    @get:PropertyName("isUser")
+    @set:PropertyName("isUser")
+    var isUser: Boolean = false,
     val timestamp: Long = 0
 )
